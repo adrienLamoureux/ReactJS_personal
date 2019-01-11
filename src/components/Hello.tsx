e09d08aaa1cd34f6d3c72fb6beca2777dad60043
adrien_lamoureux <vergil1534@gmail.com>
2019-01-11 17:50:22 +0100

    Add react-router

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,40 @@
 import * as React from "react";
+import { Button } from 'react-bootstrap';
 
+// API
+export const HelloAPI = {
+
+}
+
+// Input
 export interface HelloProps { compiler: string; framework: string; }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+// DOM
+export const Hello = (props: HelloProps) => 
+<div>
+    <h1>Hello from {props.compiler} and {props.framework}!</h1>
+    <Button bsStyle="primary" onClick={(event: React.MouseEvent<Button, MouseEvent>) => {HelloEvent.onClick(event)}}>default</Button>
+</div>;
+
+// DOM events
+const HelloEvent = {
+    
+    /**
+     * OnClick
+     * @param event 
+     */
+    onClick(event: React.MouseEvent<Button, MouseEvent>): void {
+        log("onClick"); 
+        log(event);
+    }
+}
+
+// Internal functions
+
+/**
+ * Console.log; Testing purpose
+ * @param x 
+ */
+function log(x: any) {
+    console.log(x);
+}
